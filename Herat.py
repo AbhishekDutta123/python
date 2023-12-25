e8d8d776663c74fccee69dfe71b9558dcabe8a4d
AbhishekDutta123 <86095000+AbhishekDutta123@users.noreply.github.com>
2023-12-25 23:09:36 +0530

    Add files via upload

---
+++ Herat.py
@@ -0,0 +1,18 @@
+from turtle import *
+bgcolor("black")
+color("red")
+begin_fill()
+pensize(5)
+left(70)
+forward(185)
+circle(70,200)
+right(140)
+circle(70,200)
+forward(185)
+end_fill()
+
+
+
+d={}
+d[10]="abhi"
+d[2]="Pink"
